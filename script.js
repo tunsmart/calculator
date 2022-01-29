6fca465d7097d8316217bdb71a73196a10536e95
Babatunde Alli <tunsmart@gmail.com>
2022-01-29 18:20:39 -0500

    Add the three files and create the basic havascript functions

---
+++ script.js
@@ -0,0 +1,30 @@
+const add = function(a,b) {
+	return a + b;
+};
+
+const subtract = function(a,b) {
+	return a - b;
+};
+
+const multiply = function(a,b) {
+  return a * b;
+};
+
+const divide = function(a,b) {
+    return a / b;
+  };
+
+const operate = function(operator, a, b) {
+    if (operator == '+') {
+        return add(a,b);
+    } else if (operator == '-') {
+        return subtract(a,b);
+    } else if (operator == '*') {
+        return multiply(a,b);
+    } else {
+        return divide(a,b);
+    }
+}
+
+
+
